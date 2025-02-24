542e64e8e8d011d2190f347906a62f48e9638dfd
hell0w0rld2 <43834630+hell0w0rld2@users.noreply.github.com>
2025-02-24 13:44:36 -0700

    Ready to implement with db

--- monitor/views.py
+++ monitor/views.py
@@ -7,12 +7,25 @@ from django.http import HttpResponse
 def index(request):
     if request.method == 'POST':
         login = request.POST.get('login')
-        
+        type = None
         if login.isnumeric():
-            print("Student ID Number")
+            type = 'sid'
+        elif login.isalpha():
+            type = 'name'
         else:
-            print(login.split())
+            type = 'other'
+        print(type)
+        if type == 'sid':
+            #query for sid. If it's in db, go to class select. If false, go setup
+            pass
+        elif type == 'name':
+            #query for sid. If it's in db, go to class select. If false, go to setup
+            pass
+        else:
+            return render(request, 'monitor/index.html')
+        
         return render(request, 'monitor/class_select.html')
+    
     
     return render(request, 'monitor/index.html')
 
