6dbb2fdf62091912e6ff6e522f126c4c4bde3840
hell0w0rld2 <43834630+hell0w0rld2@users.noreply.github.com>
2025-02-24 13:25:44 -0700

    Functioning sid and name input, sending to check in when successful

--- monitor/views.py
+++ monitor/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 
+
 def index(request):
+    if request.method == 'POST':
+        login = request.POST.get('login')
+        
+        if login.isnumeric():
+            print("Student ID Number")
+        else:
+            print(login.split())
+        return render(request, 'monitor/class_select.html')
+    
     return render(request, 'monitor/index.html')
 
 def success(request):
     return render(request, 'monitor/success.html')
+
 
 def class_select(request):
     if request.method == 'POST':
