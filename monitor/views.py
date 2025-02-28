0ddedcad03485b286aed4122bcff9d1ea6ae0434
hell0w0rld2 <43834630+hell0w0rld2@users.noreply.github.com>
2025-02-27 19:45:18 -0700

    Starting with queries

--- monitor/views.py
+++ monitor/views.py
@@ -2,31 +2,25 @@ from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
-
+from models import Students
 
 def index(request):
     if request.method == 'POST':
         login = request.POST.get('login')
         type = None
         if login.isnumeric():
-            type = 'sid'
+            student = Students.objects.get(western_id = login).first()
+            #Checks to see if student is in db, redirects to class select if in db, index otherwise
+            if student:
+                redirect(request, 'monitor/class_select.html')
+            else:
+                #TODO: Change to setup once created
+                redirect(request, 'monitor/index.html')
         elif login.isalpha():
             type = 'name'
         else:
-            type = 'other'
-        print(type)
-        if type == 'sid':
-            #query for sid. If it's in db, go to class select. If false, go setup
-            pass
-        elif type == 'name':
-            #query for sid. If it's in db, go to class select. If false, go to setup
-            pass
-        else:
             return render(request, 'monitor/index.html')
-        
-        return render(request, 'monitor/class_select.html')
-    
-    
+
     return render(request, 'monitor/index.html')
 
 def success(request):
